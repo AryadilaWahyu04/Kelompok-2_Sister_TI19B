b6b81cd5cea4bafe837cc54a61a73de9e537fa4f
Diaz Maulana Rizki <72059308+XDmRX@users.noreply.github.com>
2022-01-16 11:58:52 +0700

    Add files via upload

---
+++ rest ci client matkul/create.php
@@ -0,0 +1,13 @@
+<?php echo form_open_multipart('matkul/create');?>
+<table>
+    <tr><td>KODE MATKUL</td><td><?php echo form_input('kd_matkul');?></td></tr>
+    <tr><td>NAMA</td><td><?php echo form_input('nama');?></td></tr>
+    <tr><td>DOSEN</td><td><?php echo form_input('dosen');?></td></tr>
+    <tr><td>KELAS</td><td><?php echo form_input('kelas');?></td></tr>    
+    <tr><td colspan="2">
+        <?php echo form_submit('submit','Simpan');?>
+        <?php echo anchor('matkul','Kembali');?></td></tr>
+</table>
+<?php
+echo form_close();
+?>
---
+++ rest ci client matkul/list.php
@@ -0,0 +1,21 @@
+<font color="orange">
+<?php echo $this->session->flashdata('hasil'); ?>
+</font>
+<table border="1">
+    <tr><th>ID</th><th>KODE MATKUL</th><th>NAMA</th><th>DOSEN</th><th>KELAS</th><th></th></tr>
+    <?php
+    foreach ($datamatkul as $matkul){
+        echo "<tr>
+              <td>$matkul->id</td>
+              <td>$matkul->kd_matkul</td>
+              <td>$matkul->nama</td>
+              <td>$matkul->dosen</td>
+              <td>$matkul->kelas</td>
+              <td>".anchor('matkul/edit/'.$matkul->id,'Edit')."
+                  ".anchor('matkul/delete/'.$matkul->id,'Delete')."</td>
+              </tr>";
+    }
+    ?>
+</table>
+
+<a href="http://192.168.100.16/rest_ci_client/index.php/matkul/create"> + Tambah data</a>
